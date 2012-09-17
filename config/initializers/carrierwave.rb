3b4faf518f7b0cb515fae22e144c47ad8ace071c
Tyler Garlick <tjgarlick@gmail.com>
2012-09-17 14:04:28 -0600

    config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => "AKIAIAIYWGIVEO2NLUUQ",       # required
-    :aws_secret_access_key  => "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"       # required
+    :aws_access_key_id      => ENV['S3_KEY'],       # required
+    :aws_secret_access_key  => ENV['S3_SECRET']       # required
   }
   config.fog_directory  = 'sandbstorage'                     # required
 end
